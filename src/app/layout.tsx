9527c542e9daca72ea0c1a7bee31a06c580aff7f
stormnoopper <saroot.nae@gmail.com>
2024-05-17 18:47:40 +0700

    Added Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import TopMenu from "@/components/ControlSystem/TopMenu";
+import Footer from "@/components/ControlSystem/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {showTopMenu && <TopMenu/>}
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Category from '@/components/Home Page/Category';
 import Coupon from '@/components/Home Page/Coupon';
 import PromoBanner from '@/components/Home Page/PromoBanner';
 import RecommendedProducts from '@/components/Home Page/RecommendedProducts';
+import Footer from '@/components/ControlSystem/Footer';
 
 export default function Home() {
   
@@ -12,6 +13,7 @@ export default function Home() {
       <Category></Category>
       <Coupon></Coupon>
       <RecommendedProducts></RecommendedProducts>
+      
     </main>
   );
 }
